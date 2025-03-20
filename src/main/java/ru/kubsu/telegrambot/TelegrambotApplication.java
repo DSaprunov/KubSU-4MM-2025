a9f9df9015a7316c578c52a2053399db92a24258
Denis Saprunov <avelin.den@mail.ru>
2025-03-20 21:28:19 +0300

    Yml + Controller

--- src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
+++ src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
@@ -2,11 +2,13 @@ package ru.kubsu.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.kubsu.telegrambot.controller.TestController;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TelegrambotApplication.class, args);
 	}
 
